aa27aa963ab68de647a2225b13d4c9f760b67f3b
Roy Nijkamp <roy_nijkamp@hotmail.com>
2018-04-06 16:31:49 +0200

    changed example for new filename

--- example/Example.php
+++ example/Example.php
@@ -9,10 +9,11 @@ use phpGPX\phpGPX;
 require_once '../vendor/autoload.php';
 
 $gpx = new phpGPX();
-$file = $gpx->load('Evening_Ride.gpx');
+//$file = $gpx->load('Evening_Ride.gpx');
+$file = $gpx->load('test.gpx');
 
 phpGPX::$PRETTY_PRINT = true;
-$file->save('output_Evening_Ride.gpx', phpGPX::XML_FORMAT);
+//$file->save('output_Evening_Ride.gpx', phpGPX::XML_FORMAT);
 
 foreach ($file->tracks as $track) {
 	var_dump($track->stats->toArray());
